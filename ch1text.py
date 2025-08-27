3b06b19cb491368c3795423118655498c8a56974
Yehudah Deitcher <2027.y.deitcher@shalhevet.org>
2025-08-27 14:49:58 -0700

    Add description of the dusky dolphin

---
+++ ch1text.py
@@ -0,0 +1,9 @@
+text="""The dusky dolphin (Lagenorhynchus obscurus) is a small oceanic dolphin found in coastal waters of the Southern
+ Hemisphere. Major populations occur around South America, southwestern Africa, New Zealand, and several oceanic 
+ islands. It has a somewhat stocky body with a short beak, a curved dorsal fin and flippers, and a multi-coloured 
+ pigmentation of black, grey, and white. The species prefers cool currents and inshore waters. It lives in a 
+ fission–fusion society where groups change size based on social and environmental conditions. The dolphin feeds on 
+ several fish and squid species, sometimes using daytime bait ball herding and nightime feeding in deep scattering 
+ layers. Mating is polygynandrous – several males will chase after a single female. Young are raised by females in 
+ nursery groups. The dusky dolphin is known for its acrobatics, displaying leaping behaviours. It is a popular tourist 
+ attraction and the object of whale watching tours."""
